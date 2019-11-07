926f6f8fa343f4d60162d6e390fea0561920f5f9
sara-bertova <sara.bertova@gmail.com>
2019-11-07 14:48:43 +0100

    first version, barely any styling

---
+++ guessnumber.js
@@ -0,0 +1,18 @@
+var y = Math.floor(Math.random() * 50 + 1);
+var guess = 1;
+
+document.getElementById("submitguess").onclick = function () {
+
+    var x = document.getElementById("guessField").value;
+
+    if (x == y) {
+        alert("YOU ARE A GENIUS! YOU HAVE USED ONLY" +
+            guess + " GUESS/ES ");
+    } else if (x > y) {
+        guess++;
+        alert("OOPS, YOUR GUESS IS TOO HIGH. GO LOWER!");
+    } else {
+        guess++;
+        alert("OOPS, YOUR GUESS IS TOO LOW. GO HIGHER!")
+    }
+}
