b721fbf1e5bc02edc10b174ed43229cad2399a04
AdeliteSamak <55930858+AdeliteSamak@users.noreply.github.com>
2019-11-07 20:37:48 +0100

    Add files via upload
    idk how it works, i just uploaded it again like this

--- guessnumber.js
+++ guessnumber.js
@@ -6,7 +6,7 @@ document.getElementById("submitguess").onclick = function () {
     var x = document.getElementById("guessField").value;
 
     if (x == y) {
-        alert("YOU ARE A GENIUS! YOU HAVE USED ONLY" +
+        alert("YOU ARE A GENIUS! YOU HAVE USED ONLY " +
             guess + " GUESS/ES ");
     } else if (x > y) {
         guess++;
